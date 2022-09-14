c85de10cc5aa78b9bdf298140b5b310951afdacf
mkucmus <maciejkucmus@gmail.com>
2022-09-14 15:38:55 +0200

    chore: test

--- scripts/build.js
+++ scripts/build.js
@@ -2,8 +2,10 @@ import { execa } from 'execa';
 
 
 async function buildAll() {
- await execa('pnpm', ['build'], { cwd: './packages/components'}).stdout.pipe(process.stdout)
- await execa('pnpm', ['build'], { cwd: './packages/vue-cms-base-plugin'}).stdout.pipe(process.stdout)
+  await execa('pnpm', ['build'], { cwd: './packages/components'}).then(() => {
+    execa('pnpm', ['build'], { cwd: './packages/vue-cms-base-plugin'}).catch((error) => console.error('an error occured during the build.', error))
+ })
+
 }
 
 buildAll();
